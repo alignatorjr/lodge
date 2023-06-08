2087ec65d96f638e664abea86ec2eca8f22c35c9
alignatorjr <131113623+alignatorjr@users.noreply.github.com>
2023-06-07 23:28:12 -0700

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,24 +1,34 @@
 var x = 0;
 		function smokeMove(){
 			var smoke = document.getElementsByClassName("smoke");
-      		x=0;
-			while (smoke[x].style.visibility!="hidden"){
-      			x++;
-      		}
-      		var fromtop = parseInt(((document.getElementById("smokestack").style.top)).replace("vw", ""))
-      		var newfromtop = fromtop - 2;
-      		document.getElementById("smokestack").style.top = newfromtop + "vw";
-      		smoke[x].style.visibility = "visible";
-      		if(x>=8){
-      			var smokestack = document.getElementById("smokestack");
-      			var house = document.getElementById("house");
-      			var intro = document.getElementById("intro");
-      			const allintro = document.getElementById("allintrostuff");
-      			animationListener(allintrostuff);
-      			smokestack.classList.add("introanimation");
-      			house.classList.add("houseintroanimation");
-      			intro.classList.add("backgroundanimation");
-      			clearTimeout(myVar);
+			var hasCookie = checkForCookie('intro');
+			if (hasCookie == true){
+			    document.getElementById("house").remove();
+              	document.getElementById("smokestack").remove();
+              	document.getElementById("intro").remove();
+              	clearTimeout(myVar);
+			    return;
+			}
+			else{
+                x=0;
+                while (smoke[x].style.visibility!="hidden"){
+                    x++;
+                }
+                var fromtop = parseInt(((document.getElementById("smokestack").style.top)).replace("vw", ""))
+                var newfromtop = fromtop - 2;
+                document.getElementById("smokestack").style.top = newfromtop + "vw";
+                smoke[x].style.visibility = "visible";
+                if(x>=8){
+                    var smokestack = document.getElementById("smokestack");
+                    var house = document.getElementById("house");
+                    var intro = document.getElementById("intro");
+                    const allintro = document.getElementById("allintrostuff");
+                    animationListener(allintrostuff);
+                    smokestack.classList.add("introanimation");
+                    house.classList.add("houseintroanimation");
+                    intro.classList.add("backgroundanimation");
+                    clearTimeout(myVar);
+                }
       		}
       		x++;
 		}
@@ -34,10 +44,10 @@ var x = 0;
 
 		function checkForCookie(cname) {
         	if (document.cookie == 'intro=intro'){
-        		alert("no intro");
+        		return false;
         	}
         	else {
-        		alert("do intro");
+        		return true;
         	}
         }
 
